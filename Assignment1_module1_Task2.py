705ceed6131b54089cf4eb5cbc4ca8ce353f511e
Priyanshu Kumar Jha <priyanshujha996@gmail.com>
2026-01-04 17:33:26 +0530

    Delete Assignment1_module1_Task2.py

--- Assignment1_module1_Task2.py
+++
@@ -1,6 +0,0 @@
-#Assignment1_module2_Task2
-#Personalised Greeting
-Name=input("enter your first name :")
-LastName=input("enter your last name :")
-
-print('Hello,'+' '+ Name,"",LastName + "!" + ' ' + 'Welcome to python programming.')
