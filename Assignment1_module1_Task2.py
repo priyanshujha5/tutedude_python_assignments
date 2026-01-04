87b1cf413ed3c6ea27f491c0676580b253bfecca
Priyanshu Kumar Jha <priyanshujha996@gmail.com>
2026-01-04 17:31:11 +0530

    Fix spelling and formatting in greeting script

--- Assignment1_module1_Task2.py
+++ Assignment1_module1_Task2.py
@@ -1,5 +1,6 @@
 #Assignment1_module2_Task2
-#Persoanlised Greeting
+#Personalised Greeting
 Name=input("enter your first name :")
 LastName=input("enter your last name :")
+
 print('Hello,'+' '+ Name,"",LastName + "!" + ' ' + 'Welcome to python programming.')
