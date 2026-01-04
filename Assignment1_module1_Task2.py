c0cfef0805d6bc2024cb92d84b9956e33697582b
Priyanshu Kumar Jha <priyanshujha996@gmail.com>
2026-01-04 17:13:31 +0530

    Add files via upload

---
+++ Assignment1_module1_Task2.py
@@ -0,0 +1,5 @@
+#Assignment1_module2_Task2
+#Persoanlised Greeting
+Name=input("enter your first name :")
+LastName=input("enter your last name :")
+print('Hello,'+' '+ Name,"",LastName + "!" + ' ' + 'Welcome to python programming.')
