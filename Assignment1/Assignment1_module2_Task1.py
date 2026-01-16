87cee17e317684d5afbb65e27100f87d4f51d91b
Priyanshu Kumar Jha <priyanshujha996@gmail.com>
2026-01-16 23:29:49 +0530

    Add files via upload

---
+++ Assignment1/Assignment1_module2_Task1.py
@@ -0,0 +1,12 @@
+#Assignment1_module2: Python basics
+#Task1
+a=float(input("enter the first number :"))
+b=float(input("enter the second number :"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print('Addition:',c)
+print('Subtraction:',d)
+print('Multiplication:',e)
+print('Division:',f)
