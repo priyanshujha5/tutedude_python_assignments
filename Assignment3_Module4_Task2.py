ec5f03ec2108d20deef042941ce2e0aca320e8bb
Priyanshu Kumar Jha <priyanshujha996@gmail.com>
2026-01-20 12:20:01 +0530

    Add files via upload

---
+++ Assignment3_Module4_Task2.py
@@ -0,0 +1,13 @@
+import math
+if __name__== "__main__":
+    Number=int(input("Enter a number: "))
+def sqrt(number):
+    return math.sqrt(number)
+print(f"Squareroot: {sqrt(Number)}")
+def logarith(n):
+    return math.log(n)
+print(f"Logarithm: {logarith(Number)}")
+def sin(number):
+    return math.sin(number)
+print(f"Sin: {sin(Number)}")
+print("Thank you for using this program")
