cdcd195e1bb8b7f1d60a317d2f1ba2ed7bd14bc7
Priyanshu Kumar Jha <priyanshujha996@gmail.com>
2026-02-01 16:05:36 +0530

    Delete Assignment5/Assignment5_module6_Task1.py

--- Assignment5/Assignment5_module6_Task1.py
+++
@@ -1,9 +0,0 @@
-student_data = {"Rahul": 85,"Roy": 92,"Shaurya": 78,"Rana": 95}
-
-name = input("Enter the student's name: ")
-
-if name in student_data:
-    print(f"{name}'s marks: {student_data[name]}")
-else:
-    print("Student not found.")
-
